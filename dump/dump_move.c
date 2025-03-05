b31fdb6bdff36bc9cbfd4ebd6c2520ab8fb88e0c
Sergey Litvinov <slitvinov@gmail.com>
2025-03-05 18:03:06 +0100

    from_names

--- dump/dump_move.c
+++ dump/dump_move.c
@@ -23,7 +23,7 @@ struct DumpHeader {
   int i, depth, npe, version;
   struct coord n;
 };
-static struct DumpHeader from_header;
+static struct DumpHeader from_header, to_header;
 static double *values;
 static long traverse(int);
 static double X0, Y0, Z0, L0;
@@ -32,7 +32,7 @@ int main(int argc, char **argv) {
   long i;
   unsigned len;
   double o[4];
-  char **names;
+  char **from_names, *to_name;
   while (*++argv != NULL && argv[0][0] == '-')
     switch (argv[0][1]) {
     case 'h':
@@ -62,6 +62,7 @@ int main(int argc, char **argv) {
     fprintf(stderr, "dump_move: error: output.dump is not given\n");
     exit(1);
   }
+  argv++;
 
   if ((from_file = fopen(from_path, "r")) == NULL) {
     fprintf(stderr, "dump_move: error: fail to open '%s'\n", from_path);
@@ -87,18 +88,19 @@ int main(int argc, char **argv) {
           "  depth:          multigrid depth: %d\n"
           "      i:     simulation iteration: %d\n"
           "      n: multigrid MPI dimensions: [%g %g %g]\n",
-          from_header.version, from_header.t, from_header.len, from_header.npe, from_header.depth,
-          from_header.i, from_header.n.x, from_header.n.y, from_header.n.z);
-  if ((names = malloc(from_header.len * sizeof *names)) == NULL) {
+          from_header.version, from_header.t, from_header.len, from_header.npe,
+          from_header.depth, from_header.i, from_header.n.x, from_header.n.y,
+          from_header.n.z);
+  if ((from_names = malloc(from_header.len * sizeof *from_names)) == NULL) {
     fprintf(stderr, "dump_move: error: malloc failed\n");
     exit(1);
   }
   for (i = 0; i < from_header.len; i++) {
     FREAD(&len, sizeof len, 1, from_file, from_path);
-    names[i] = malloc((len + 1) * sizeof *names[i]);
-    FREAD(names[i], sizeof *names[i], len, from_file, from_path);
-    names[i][len] = '\0';
-    fprintf(stderr, "name: %s\n", names[i]);
+    from_names[i] = malloc((len + 1) * sizeof *from_names[i]);
+    FREAD(from_names[i], sizeof *from_names[i], len, from_file, from_path);
+    from_names[i][len] = '\0';
+    fprintf(stderr, "name: %s\n", from_names[i]);
   }
   FREAD(o, sizeof o, 1, from_file, from_path);
   fprintf(stderr,
@@ -109,6 +111,39 @@ int main(int argc, char **argv) {
   Y0 = o[1];
   Z0 = o[2];
   L0 = o[3];
+  FREAD(&to_header, sizeof to_header, 1, to_file, to_path);
+  if (fwrite(&from_header, sizeof(from_header), 1, output_file) != 1) {
+    fprintf(stderr, "dump_move: error: fail to write '%s'\n", output_path);
+    exit(1);
+  }
+  assert(from_header.len == to_header.len);
+  assert(from_header.version == to_header.version);
+  for (i = 0; i < to_header.len; i++) {
+    FREAD(&len, sizeof len, 1, to_file, to_path);
+    to_name = malloc((len + 1) * sizeof *to_name);
+    FREAD(to_name, sizeof *to_name, len, to_file, to_path);
+    to_name[len] = '\0';
+
+    if (fwrite(&len, sizeof len, 1, output_file) != 1) {
+      fprintf(stderr, "dump_move: error: fail to write '%s'\n", output_path);
+      exit(1);
+    }
+    if (fwrite(to_name, sizeof *to_name, len, output_file) != len) {
+      fprintf(stderr, "dump_move: error: fail to write '%s'\n", output_path);
+      exit(1);
+    }
+    assert(strcmp(to_name, from_names[i]) == 0);
+    free(to_name);
+  }
+  FREAD(o, sizeof o, 1, to_file, to_path);
+  assert(X0 == o[0]);
+  assert(Y0 == o[1]);
+  assert(Z0 == o[2]);
+  assert(L0 == o[3]);
+  if (fwrite(o, sizeof o, 1, output_file) != 1) {
+    fprintf(stderr, "dump_move: error: fail to write '%s'\n", output_path);
+    exit(1);
+  }
   if ((values = malloc(from_header.len * sizeof *values)) == NULL) {
     fprintf(stderr, "dump_move: error: malloc failed\n");
     exit(1);
@@ -117,8 +152,8 @@ int main(int argc, char **argv) {
   traverse(0);
   fprintf(stderr, "nleaf: %ld\n", nleaf);
   for (i = 0; i < from_header.len; i++)
-    free(names[i]);
-  free(names);
+    free(from_names[i]);
+  free(from_names);
   if (fclose(from_file) != 0) {
     fprintf(stderr, "dump_move: error: fail to close '%s'\n", from_path);
     exit(1);
@@ -139,7 +174,8 @@ static long traverse(int level) {
   long size, size0;
 
   if (fread(&flags, sizeof flags, 1, from_file) != 1 ||
-      fread(values, sizeof *values, from_header.len, from_file) != from_header.len) {
+      fread(values, sizeof *values, from_header.len, from_file) !=
+          from_header.len) {
     fprintf(stderr, "dump_move: fail to read '%s' at level '%d'\n", from_path,
             level);
     exit(1);
