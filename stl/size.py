b7988e8d457a97341c3ad3c0674eecc29c024c34
Sergey Litvinov <slitvinov@gmail.com>
2025-03-05 11:27:44 +0100

    yapf -i

--- stl/size.py
+++ stl/size.py
@@ -6,6 +6,7 @@ import struct
 import os
 import argparse
 
+
 def read(path):
     with open(path, "rb") as file:
         mm = mmap.mmap(file.fileno(), length=0, access=mmap.ACCESS_READ)
@@ -13,7 +14,9 @@ def read(path):
         return np.ndarray((nt, 3, 3), np.dtype("<f4"), mm, 80 + 4 + 12,
                           (36 + 12 + 2, 12, 4))
 
-parser = argparse.ArgumentParser(description="Print the sizes of the STL file.")
+
+parser = argparse.ArgumentParser(
+    description="Print the sizes of the STL file.")
 parser.add_argument("input", type=str, help="Path to the input STL file")
 args = parser.parse_args()
 r = read(args.input)
