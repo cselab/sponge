43f2efaf42511abe5368d4c90b897701d5467a3d
Sergey Litvinov <slitvinov@gmail.com>
2025-03-05 11:17:04 +0100

    size.py: minor

--- cylinder/stl/size.py
+++ cylinder/stl/size.py
@@ -9,7 +9,6 @@ import argparse
 def read(path):
     with open(path, "rb") as file:
         mm = mmap.mmap(file.fileno(), length=0, access=mmap.ACCESS_READ)
-        print(hdr)
         nt, = struct.unpack('<i', mm[80:80 + 4])
         return np.ndarray((nt, 3, 3), np.dtype("<f4"), mm, 80 + 4 + 12,
                           (36 + 12 + 2, 12, 4))
