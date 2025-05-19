8afe480ef62169fc8e7e9cdb5d43e3d01827f73d
Sergey Litvinov <slitvinov@gmail.com>
2025-05-19 10:51:51 +0200

    run local

--- cylinder/deploy/run.sh
+++ cylinder/deploy/run.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-l=8
-m=11
+l=5 m=6
 (cd ../../dump && make dump2xdmf stl2dump)
-#../stl/cylinder.py
- ../stl/center.py $HOME/cfd.stl
-../paraview/stl2dump -o -v -- -5 -6.25 -6.25 12.5  $l $m  64 center.stl basilisk.dump
-../paraview/dump2xdmf basilisk.dump basilisk
-# exec mpiexec ./cylinder -v -r 220 -l 6 -m 10 -p 100 -e 3000 -d basilisk.dump -o h
+../../stl/cylinder.py ver.stl
+../../stl/center.py ver.stl center.stl
+zlim=`../../stl/size.py center.stl`
+../../dump/stl2dump -o -v -w z $((m-1)) $zlim -- \
+		    -5 -6.25 -6.25 12.5  $l $m  64 center.stl basilisk.dump
+set -- $zlim
+z=$2
+mpiexec ./cylinder -v -i -Z $z \
+    	 -r 2000 -l $l -m $m -p 100 -e 200 -f force.dat -d basilisk.dump -o h -b pp
