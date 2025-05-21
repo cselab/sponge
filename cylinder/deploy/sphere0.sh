e70c47bf9cf61f8c1026ff29ec2032f0b0c69273
Sergey Litvinov <slitvinov@gmail.com>
2025-05-21 17:53:21 +0200

    k=

--- cylinder/deploy/sphere0.sh
+++ cylinder/deploy/sphere0.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 xargs --process-slot-var I -n 1 -P 4 sh -xc '
 n=64 l=7 m=11
-i=$((n*I/4)) j=$((n*(I+1)/4-1))
+i=$((n*I/4)) j=$((n*(I+1)/4-1)) k=$((n/4))
 mkdir -p $0
 cd $0 &&
-   mpiexec --bind-to core --cpu-set $i-$j ../cylinder \
+   mpiexec -n $k --bind-to core --cpu-set $i-$j ../cylinder \
    -v -r 100 -l $l -m $m -p 100 -e 10 -f force.dat -o h -b pp -S sphere -z 12.5 >stdout 2>stderr
    echo $? > status
 ' <<'!'
