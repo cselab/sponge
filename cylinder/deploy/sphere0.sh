b1a4ed4cbbb028148a84c34145b3031a37727bc7
Sergey Litvinov <slitvinov@gmail.com>
2025-05-21 18:45:30 +0200

    --bind-to-core

--- cylinder/deploy/sphere0.sh
+++ cylinder/deploy/sphere0.sh
@@ -5,7 +5,7 @@ i=$((n*I/4)) j=$((n*(I+1)/4-1)) k=$((n/2/4))
 mkdir -p $0
 cd $0 &&
    taskset --cpu-list $i-$j \
-   mpiexec -n $k --bind-to core --map-by core --report-bindings ../cylinder \
+   mpiexec -n $k --report-bindings ../cylinder \
    -v -r 100 -l $l -m $m -p 100 -e 10 -f force.dat -o h -b pp -S sphere -z 12.5 >stdout 2>stderr
    echo $? > status
 ' <<'!'
