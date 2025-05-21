fb38f22b33510d020ed36dcaa54f8e2b8efa6fbf
Sergey Litvinov <slitvinov@gmail.com>
2025-05-21 18:04:47 +0200

    up

--- cylinder/deploy/sphere0.sh
+++ cylinder/deploy/sphere0.sh
@@ -4,7 +4,7 @@ n=64 l=7 m=11
 i=$((n*I/4)) j=$((n*(I+1)/4-1)) k=$((n/4))
 mkdir -p $0
 cd $0 &&
-   taskset --cpu-set $i-$j \
+   taskset --cpu-list $i-$j \
    mpiexec -n $k --report-bindings ../cylinder \
    -v -r 100 -l $l -m $m -p 100 -e 10 -f force.dat -o h -b pp -S sphere -z 12.5 >stdout 2>stderr
    echo $? > status
