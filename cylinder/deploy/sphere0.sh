4aeee2a78ebd77f4df1c81dd74656ee299c1cf20
Sergey Litvinov <slitvinov@gmail.com>
2025-05-21 19:16:49 +0200

    up

--- cylinder/deploy/sphere0.sh
+++ cylinder/deploy/sphere0.sh
@@ -4,7 +4,7 @@ n=128 l=7 m=11 k=16
 re=$0
 mkdir -p $re
 cd $0 &&
-   mpiexec -n $k --map-by core:PE=2 \
+   mpiexec -n $k --map-by slot:PE=2 \
    --report-bindings ../cylinder \
    -v -r $re -l $l -m $m -p 100 -e 10 -f force.dat -o h -b pp -S sphere -z 12.5 >stdout 2>stderr
    echo $? > status
