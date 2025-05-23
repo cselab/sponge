c3cca18915c6cb8326549284cd8e9823a88a3d78
Sergey Litvinov <slitvinov@gmail.com>
2025-05-23 11:43:52 +0200

    NITERMAX = 20

--- cylinder/cylinder.c
+++ cylinder/cylinder.c
@@ -121,7 +121,7 @@ static scalar l2[];
 static vector omega[];
 static scalar phi[];
 int main(int argc, char **argv) {
-  NITERMAX = 10;
+  NITERMAX = 20;
   char *end;
   const char *boundaries;
   int ReynoldsFlag, MaxLevelFlag, MinLevelFlag, PeriodFlag, TendFlag,
