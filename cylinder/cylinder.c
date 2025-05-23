fa9e238159639f3a6ac729308b3c254c78aa4faa
Sergey Litvinov <slitvinov@gmail.com>
2025-05-23 10:33:12 +0200

    comment fm = fs

--- cylinder/cylinder.c
+++ cylinder/cylinder.c
@@ -486,8 +486,8 @@ event init(t = 0) {
   u.n[embed] = dirichlet(0);
   u.t[embed] = dirichlet(0);
   u.r[embed] = dirichlet(0);
-  fm = fs;
-  cm = cs;
+  /* fm = fs;
+     cm = cs; */
 }
 
 event properties(i++) { foreach_face() muv.x[] = fm.x[] / reynolds; }
