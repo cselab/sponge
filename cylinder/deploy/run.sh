67f4c36fb805b44933168ddff7bd0dea2237860d
Sergey Litvinov <slitvinov@gmail.com>
2025-05-23 12:16:49 +0200

    -p 10

--- cylinder/deploy/run.sh
+++ cylinder/deploy/run.sh
@@ -19,4 +19,4 @@ then
 			-5 -6.25 -6.25 12.5  $l $m  64 center.stl basilisk.$l.$m.dump
 fi
 $mpiexec ./cylinder -v \
-    	 -r 2000 -l $l -m $m -p 1 -e 200 -f force.dat -d basilisk.$l.$m.dump -o h -b pp
+    	 -r 2000 -l $l -m $m -p 10 -e 200 -f force.dat -d basilisk.$l.$m.dump -o h -b pp
