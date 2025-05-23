4db9ad4ae4247ddcbabbfa0950853b7b722a8423
Sergey Litvinov <slitvinov@gmail.com>
2025-05-23 09:52:18 +0200

    use $l and $m in file names

--- cylinder/deploy/run.sh
+++ cylinder/deploy/run.sh
@@ -2,15 +2,15 @@
 set -eu
 
 #l=9 m=12
-l=7 m=10
+l=7 m=11
 make
 (cd ../../dump && make dump2xdmf stl2dump)
-# ../../stl/cylinder.py -n 64 ver.stl
-# ../../stl/center.py ver.stl center.stl
+../../stl/cylinder.py -n 64 ver.stl
+../../stl/center.py ver.stl center.stl
 zlim=`../../stl/size.py center.stl`
 ../../dump/stl2dump -v -s 26 -o -w z $((m-1)) $zlim -- \
-		    -5 -6.25 -6.25 12.5  $l $m  64 center.stl basilisk.dump
+		    -5 -6.25 -6.25 12.5  $l $m  64 center.stl basilisk.$l.$m.dump
 set -- $zlim
 zlim=$2
 mpiexec.openmpi ./cylinder -v -i -Z $zlim \
-    	 -r 2000 -l $l -m $m -p 1 -e 200 -f force.dat -d basilisk.dump -o h -b pp
+    	 -r 2000 -l $l -m $m -p 1 -e 200 -f force.dat -d basilisk.$l.$m.dump -o h -b pp
