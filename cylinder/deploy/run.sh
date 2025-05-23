a52157afc17fd8f4f40cb901c0484f3e8f4931ce
Sergey Litvinov <slitvinov@gmail.com>
2025-05-23 09:59:53 +0200

    l=7, m=10

--- cylinder/deploy/run.sh
+++ cylinder/deploy/run.sh
@@ -2,14 +2,18 @@
 set -eu
 
 #l=9 m=12
-l=7 m=11
+: ${l=7}
+: ${m=10}
 make
 (cd ../../dump && make dump2xdmf stl2dump)
 ../../stl/cylinder.py -n 64 ver.stl
 ../../stl/center.py ver.stl center.stl
 zlim=`../../stl/size.py center.stl`
-../../dump/stl2dump -v -s 26 -o -w z $((m-1)) $zlim -- \
-		    -5 -6.25 -6.25 12.5  $l $m  64 center.stl basilisk.$l.$m.dump
+if test ! -f basilisk.$l.$m.dump
+then
+    ../../dump/stl2dump -v -s 26 -o -w z $((m-1)) $zlim -- \
+			-5 -6.25 -6.25 12.5  $l $m  64 center.stl basilisk.$l.$m.dump
+fi
 set -- $zlim
 zlim=$2
 mpiexec.openmpi ./cylinder -v -i -Z $zlim \
