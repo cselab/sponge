9d1cc0f0f418eacfbc86247a3ce6d9650c21edbc
Sergey Litvinov <slitvinov@gmail.com>
2025-05-23 10:17:53 +0200

    up

--- cylinder/deploy/dbg.sh
+++ cylinder/deploy/dbg.sh
@@ -1,2 +1,2 @@
 cd ..
-CC99=mpicc $HOME/.locla/bin/qcc -disable-dimensions -debug -events -D_MPI=1 -DTRASH=1 cylinder.c -o deploy/cylinder -lm -O0 -g3
+CC99=mpicc $HOME/.local/bin/qcc -disable-dimensions -debug -events -D_MPI=1 -DTRASH=1 cylinder.c -o deploy/cylinder -lm -O0 -g3
